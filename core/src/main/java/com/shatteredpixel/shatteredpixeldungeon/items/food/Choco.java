7471f1c05a90146a9a132519845fa14709f5a382
chocosuki <chocosuki81@gmail.com>
2025-10-10 01:09:03 +0800

    为解决冲突加注释后面删掉

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
@@ -1,9 +1,22 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
 
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+//import com.shatteredpixel.shatteredpixeldungeon.utils.Holidays;
+//暂时标注内容，后面还会改
 
 public class Choco extends Food {
     {
         image = ItemSpriteSheet.CHOCO;
+        bones = false;
+
     }
-}
+    @Override
+	public int value() {
+        //if(Holidays.holiday == Holidays.Holiday.midAutumnFestival){
+        // return 100*quantity;
+       // } else {
+        return 50*quantity;
+        }
+	}
+    
+//}
