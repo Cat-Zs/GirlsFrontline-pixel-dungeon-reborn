e704bdfc4811eaaf97e86d8760876f698284a71b
chocosuki <chocosuki81@gmail.com>
2025-10-10 03:18:12 +0800

    好像加回来也没什么事，那后续就不搞嵌入了直接引用这个农历就好

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
@@ -1,7 +1,7 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
 
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
-//import com.shatteredpixel.shatteredpixeldungeon.utils.Holidays;
+import com.shatteredpixel.shatteredpixeldungeon.utils.Holidays;
 //暂时标注内容，后面还会改
 
 public class Choco extends Food {
@@ -10,13 +10,14 @@ public class Choco extends Food {
         bones = false;
 
     }
+    
     @Override
 	public int value() {
-        //if(Holidays.holiday == Holidays.Holiday.midAutumnFestival){
-        // return 100*quantity;
-       // } else {
+        if(Holidays.holiday == Holidays.Holiday.midAutumnFestival){
+         return 100*quantity;
+        } else {
         return 50*quantity;
         }
 	}
     
-//}
+}
