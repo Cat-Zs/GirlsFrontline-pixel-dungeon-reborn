5e2fc0a7d25278acf4447fcf26aa583559c5cb4a
chocosuki <chocosuki81@gmail.com>
2025-10-10 04:57:20 +0800

    添加了鼠王在中秋给月饼的设定，但是没经过反复地具体测验

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/food/Choco.java
@@ -1,23 +1,49 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.food;
-
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 import com.shatteredpixel.shatteredpixeldungeon.utils.Holidays;
-//暂时标注内容，后面还会改
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 
 public class Choco extends Food {
     {
-        image = ItemSpriteSheet.CHOCO;
+        reset();
         bones = false;
-
     }
     
     @Override
-	public int value() {
-        if(Holidays.holiday == Holidays.Holiday.midAutumnFestival){
-         return 100*quantity;
+    public void reset() {
+        super.reset();
+        if (Holidays.holiday == Holidays.Holiday.midAutumnFestival) {
+            image = ItemSpriteSheet.SALTYMOONCAKE;
         } else {
-        return 50*quantity;
+            image = ItemSpriteSheet.CHOCO;
         }
-	}
+    }
+    
+    @Override
+    public String name() {
+        if (Holidays.holiday == Holidays.Holiday.midAutumnFestival) {
+            return Messages.get(this, "salty_moon_cake");
+        } else {
+            return Messages.get(this, "choco");
+        }
+    }
+    
+    @Override
+    public String info() {
+        if (Holidays.holiday == Holidays.Holiday.midAutumnFestival) {
+            return Messages.get(this, "salty_moon_cake_desc");
+        } else {
+            return Messages.get(this, "choco_desc");
+        }
+    }
+    
+    @Override
+    public int value() {
+        if(Holidays.holiday == Holidays.Holiday.midAutumnFestival){
+            return 100*quantity;
+        } else {
+            return 50*quantity;
+        }
+    }
     
 }
