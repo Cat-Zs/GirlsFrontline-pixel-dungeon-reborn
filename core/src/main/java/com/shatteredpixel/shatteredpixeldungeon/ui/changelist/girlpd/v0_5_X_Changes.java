9c30065a51ee30be4f246ae1414530b8d6fc4147
chocosuki <chocosuki81@gmail.com>
2025-10-14 18:48:04 +0800

    小更改

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/girlpd/v0_5_X_Changes.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/changelist/girlpd/v0_5_X_Changes.java
@@ -3,6 +3,7 @@ package com.shatteredpixel.shatteredpixeldungeon.ui.changelist.girlpd;
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.shatteredpixel.shatteredpixeldungeon.Badges;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroClass;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Goo;
 import com.shatteredpixel.shatteredpixeldungeon.effects.BadgeBanner;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.ChangesScene;
@@ -53,11 +54,14 @@ public class v0_5_X_Changes {
             "_-_ 瞄准模式下，攻击的伤害区间为武器最高伤害的85%-120%，且精准度提升"
         ));
 
-        changes.addButton(new ChangeButton(new ItemSprite(ItemSpriteSheet.REDBOOK, null), "红色语录",
-            "_-_ 新增了可以消耗1点充能自己阅读语录，同时获得短时间的祝福效果\n"+
-            "_-_ 对语录升级条件进行了补充说明，便于玩家理解并实现遗物升级"
+        changes.addButton(new ChangeButton(new ItemSprite(ItemSpriteSheet.REDBOOK, null), "袖珍本",
+            "_-_ 新增了可以消耗1点充能自己阅读袖珍本，同时获得短时间的祝福效果\n"+
+            "_-_ 对袖珍本升级条件进行了补充说明，便于玩家理解并实现遗物升级"
         ));
-       
+        changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.ARTIFACT_ROSE3, null), "刺刀",
+			"_-_ 刺刀增强，升级时可以为AR15提供少量恢复"
+		 ));
+
     changes = new ChangeInfo(Messages.get(ChangesScene.class, "changes"), false, null);
 		changes.hardlight( CharSprite.WARNING );
 		changeInfos.add(changes);
@@ -98,6 +102,9 @@ public class v0_5_X_Changes {
             "_-_ 56-1式重新获得了在饥肠辘辘状态下力量-1的特性\n"+
             "_-_ 该特性仅在角色力量大于12时生效"
         ));
+        changes.addButton(new ChangeButton(new ItemSprite(ItemSpriteSheet.GLAIVE, null), "M99",
+            "_-_ 根据描述改动，m99无法偷袭已经实装"
+        ));
     }    
 
     public static void add_0_5_4_1_Changes( ArrayList<ChangeInfo> changeInfos ){
@@ -115,10 +122,12 @@ public class v0_5_X_Changes {
 			"_-_ 增加了角色饱食度指示UI，现在角色的饱食度数值可视化了。\n"+
 			"_-_ FNC带着格里芬的补给来到了地牢！与其对话将能获取一些有趣的补给！\n"+
 			"_-_ 新增了一份节日彩蛋！\n"+
-			"_-_ 全新武器_GSh-18_加入。\n"+
-			"_-_ 为_灵刀·樱吹雪_添加了新技能，使其不再只是一把‘刀’\n"
+			"_-_ 全新武器_GSh-18_加入。\n"
 		));
-
+		changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.GREATAXE,null), "灵刀·樱吹雪",
+			"_-_ 灵刀·樱吹雪添加了新技能，使其不再只是一把‘刀’\n"+
+			"_-_ 新增了_灵刀·樱吹雪_的武器技能_居合斩_，在瞄准模式下将可以更快的进行强力攻击\n"
+		));
 		changes = new ChangeInfo(Messages.get(ChangesScene.class, "changes"), false, null);
 		changes.hardlight( CharSprite.WARNING );
 		changeInfos.add(changes);
@@ -133,6 +142,12 @@ public class v0_5_X_Changes {
 			"_-_ 因为一些特殊目的，删除了_塌方陷阱_。\n"+
 			"_-_ 强化了_Type 56-2_武器榴弹的伤害。\n"+
 			"_-_ 修正了大量文本错误，对部分文本进行了优化。\n"
+        ));
+		Image tp = new Image(new TyphoonSprite.TyphoonSpriteRe());
+		tp.scale.set(PixelScene.align(0.30f));
+		changes.addButton( new ChangeButton(tp, "提丰",
+			"_-_ 将提丰的生成率改回了在全局28-29层中有1%刷新率，而不是之前的替换28-29楼层初始怪组%。\n"+
+			"_-_ 这也意味着提丰现在在地牢中出现的频率会更高了。"
         ));
     }    
 
