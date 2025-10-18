ffe42bbb8f62238271c6db3a945829c8e49a6572
chocosuki <chocosuki81@gmail.com>
2025-10-19 06:01:51 +0800

    int了兔子武器

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
@@ -125,6 +125,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.DMR.M99;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.DMR.Sass;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.DMR.AN94;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MagesStaff;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Cypros;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Gun561;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Gun562;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.HB.Kriss;
@@ -376,8 +377,9 @@ public class Generator {
 					M1a1.class,
 					G36.class,
 					NagantRevolver.class,
+					Cypros.class
 			};
-			WEP_T2.probs = new float[]{ 5, 4, 5, 4, 4, 4 };
+			WEP_T2.probs = new float[]{ 5, 4, 5, 4, 4, 4 ,0 };
 			
 			WEP_T3.classes = new Class<?>[]{
 					Ks23.class,
