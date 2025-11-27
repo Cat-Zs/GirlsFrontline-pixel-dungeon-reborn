29de9d89e60c09e29ede68e391267535fa8c68ad
ONWARDnan <1793418102@qq.com>
2025-11-28 06:11:14 +0800

    商人跑路机制调整
    商人在玩家视野内，受伤、获得恶性buff时，才会触发跑路机制，且优先计数，第一次警告，第二次才跑路

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
@@ -23,9 +23,12 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.BlobImmunity;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.effects.particles.ElmoParticle;
 import com.shatteredpixel.shatteredpixeldungeon.items.Heap;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
@@ -35,42 +38,88 @@ import com.shatteredpixel.shatteredpixeldungeon.journal.Notes;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ShopkeeperSprite;
+import com.shatteredpixel.shatteredpixeldungeon.utils.BArray;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndBag;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndTradeItem;
 import com.watabou.noosa.Game;
 import com.watabou.utils.Callback;
+import com.watabou.utils.PathFinder;
+
+import java.util.ArrayList;
 
 public class Shopkeeper extends NPC {
 
-	{
-		spriteClass = ShopkeeperSprite.class;
+    private int turnsSinceHarmed;
 
+    {
+		spriteClass = ShopkeeperSprite.class;
+        turnsSinceHarmed = -1;
 		properties.add(Property.IMMOVABLE);
 	}
-	
-	@Override
-	protected boolean act() {
 
-		if (Dungeon.level.heroFOV[pos]){
-			Notes.add(Notes.Landmark.SHOP);
-		}
-		
-		sprite.turnTo( pos, Dungeon.hero.pos );
-		spend( TICK );
-		return super.act();
-	}
-	
-	@Override
-	public void damage( int dmg, Object src ) {
-		flee();
-	}
-	
-	@Override
-	public void add( Buff buff ) {
-		flee();
-	}
-	
-	public void flee() {
+
+    protected boolean act() {
+        if (this.turnsSinceHarmed >= 0) {
+            ++this.turnsSinceHarmed;
+        }
+
+        this.sprite.turnTo(this.pos, Dungeon.hero.pos);
+        this.spend(1.0F);
+        return super.act();
+    }
+
+    public void damage(int dmg, Object src) {
+        this.processHarm();
+    }
+
+    public void add(Buff buff) {
+        if (buff.type == Buff.buffType.NEGATIVE) {
+            this.processHarm();
+        }else{
+            super.add(buff);
+        }
+    }
+    public void processHarm() {
+        if (Dungeon.level.heroFOV[this.pos]) {
+            if (this.turnsSinceHarmed == -1) {
+                this.turnsSinceHarmed = 0;
+                this.yell(Messages.get(this, "warn", new Object[0]));
+                ArrayList<Blob> blobs = new ArrayList();
+
+                for(Class c : (new BlobImmunity()).immunities()) {
+                    Blob b = Dungeon.level.blobs.get(c);
+                    if (b != null && b.volume > 0) {
+                        blobs.add(b);
+                    }
+                }
+
+                PathFinder.buildDistanceMap(this.pos, BArray.not(Dungeon.level.solid, (boolean[])null), 4);
+
+                for(int i = 0; i < Dungeon.level.length(); ++i) {
+                    if (PathFinder.distance[i] < Integer.MAX_VALUE) {
+                        boolean affected = false;
+
+                        for(Blob blob : blobs) {
+                            if (blob.cur[i] > 0) {
+                                blob.clear(i);
+                                affected = true;
+                            }
+                        }
+
+                        if (affected && Dungeon.level.heroFOV[i]) {
+                            CellEmitter.get(i).burst(Speck.factory(101), 2);
+                        }
+                    }
+                }
+            } else if (this.turnsSinceHarmed >= 1) {
+                this.flee();
+            }
+
+        }
+    }
+
+
+    public void flee() {
 		destroy();
 
 		Notes.remove(Notes.Landmark.SHOP);
