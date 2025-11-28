bb8ad8693139ef16df02d0ceba0c6c3c4c9b4726
ONWARDnan <1793418102@qq.com>
2025-11-28 13:26:59 +0800

    商人跑路机制调整及赎回物品价格
    只保留一次承伤机会，第二次承伤或受到负面buff就跑

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Shopkeeper.java
@@ -23,12 +23,9 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs;
 
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.BlobImmunity;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.effects.CellEmitter;
-import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.effects.particles.ElmoParticle;
 import com.shatteredpixel.shatteredpixeldungeon.items.Heap;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
@@ -38,14 +35,12 @@ import com.shatteredpixel.shatteredpixeldungeon.journal.Notes;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ShopkeeperSprite;
-import com.shatteredpixel.shatteredpixeldungeon.utils.BArray;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndBag;
 import com.shatteredpixel.shatteredpixeldungeon.windows.WndTradeItem;
 import com.watabou.noosa.Game;
 import com.watabou.utils.Callback;
 import com.watabou.utils.PathFinder;
-
-import java.util.ArrayList;
+import com.watabou.utils.Random;
 
 public class Shopkeeper extends NPC {
 
@@ -62,7 +57,6 @@ public class Shopkeeper extends NPC {
         if (this.turnsSinceHarmed >= 0) {
             ++this.turnsSinceHarmed;
         }
-
         this.sprite.turnTo(this.pos, Dungeon.hero.pos);
         this.spend(1.0F);
         return super.act();
@@ -80,44 +74,13 @@ public class Shopkeeper extends NPC {
         }
     }
     public void processHarm() {
-        if (Dungeon.level.heroFOV[this.pos]) {
-            if (this.turnsSinceHarmed == -1) {
+            if (this.turnsSinceHarmed < 1) {
                 this.turnsSinceHarmed = 0;
-                this.yell(Messages.get(this, "warn", new Object[0]));
-                ArrayList<Blob> blobs = new ArrayList();
-
-                for(Class c : (new BlobImmunity()).immunities()) {
-                    Blob b = Dungeon.level.blobs.get(c);
-                    if (b != null && b.volume > 0) {
-                        blobs.add(b);
-                    }
-                }
-
-                PathFinder.buildDistanceMap(this.pos, BArray.not(Dungeon.level.solid, (boolean[])null), 4);
-
-                for(int i = 0; i < Dungeon.level.length(); ++i) {
-                    if (PathFinder.distance[i] < Integer.MAX_VALUE) {
-                        boolean affected = false;
-
-                        for(Blob blob : blobs) {
-                            if (blob.cur[i] > 0) {
-                                blob.clear(i);
-                                affected = true;
-                            }
-                        }
-
-                        if (affected && Dungeon.level.heroFOV[i]) {
-                            CellEmitter.get(i).burst(Speck.factory(101), 2);
-                        }
-                    }
-                }
-            } else if (this.turnsSinceHarmed >= 1) {
+                this.yell(Messages.get(this, "warn"));
+            }else {
                 this.flee();
             }
-
-        }
     }
-
 
     public void flee() {
 		destroy();
@@ -157,8 +120,13 @@ public class Shopkeeper extends NPC {
 		&& item instanceof Food){
 			rate-=0.2f*Dungeon.hero.pointsInTalent(Talent.BARGAIN_SKILLS);
 		}
+        int pay = Math.max(item.value()*2,(int)(item.value()*rate*(Dungeon.curDepth()+4f)));
 
-		return Math.max(item.value()*2,(int)(item.value()*rate*(Dungeon.curDepth()+4f)));
+        if(item.selled){
+            return item.value()*10;
+        }
+
+		return pay;
 	}
 	
 	public static WndBag sell() {
