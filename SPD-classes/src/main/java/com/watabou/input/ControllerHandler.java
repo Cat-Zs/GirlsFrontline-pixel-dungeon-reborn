aec1867b7a2876bc0eeb0092c1dc56c852cd0329
JDSALing <2735951230@qq.com>
2025-12-13 17:45:44 +0800

    【底层】文本输入/监听 架构升级

--- SPD-classes/src/main/java/com/watabou/input/ControllerHandler.java
+++ SPD-classes/src/main/java/com/watabou/input/ControllerHandler.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2022 Evan Debenham
+ * Copyright (C) 2014-2024 Evan Debenham
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,180 +21,272 @@
 
 package com.watabou.input;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.ControllerMapping;
+import com.badlogic.gdx.controllers.Controllers;
+import com.watabou.noosa.ui.Cursor;
 import com.watabou.utils.DeviceCompat;
 import com.watabou.utils.PointF;
 
 public class ControllerHandler implements ControllerListener {
 
-	public enum ControllerType {
-		XBOX,
-		PLAYSTATION,
-		NINTENDO,
-		OTHER
-	}
+    public enum ControllerType {
+        XBOX,
+        PLAYSTATION,
+        NINTENDO,
+        OTHER
+    }
 
-	public static ControllerType lastUsedType = ControllerType.OTHER;
+    public static ControllerType lastUsedType = ControllerType.OTHER;
+    public static boolean controllerActive = false;
 
-	private static void setControllerType(Controller controller){
-		if (controller.getName().contains("Xbox")){
-			lastUsedType = ControllerType.XBOX;
-		} else if (controller.getName().contains("PS")){
-			lastUsedType = ControllerType.PLAYSTATION;
-		} else if (controller.getName().contains("Nintendo")){
-			lastUsedType = ControllerType.NINTENDO;
-		} else {
-			lastUsedType = ControllerType.OTHER;
-		}
-	}
+    //sufficiently large number so that it'll never collide with touch pointers (which start at 0)
+    //19 is the max to avoid array overflow when interacting with some libGDX graphics objects
+    public static final int CONTROLLER_POINTER_ID = 19;
 
-	public static boolean controllersSupported() {
-		if (DeviceCompat.isAndroid() && Gdx.app.getVersion() < 16) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    private static void setControllerType(Controller controller){
+        if (controller.getName().contains("Xbox")){
+            lastUsedType = ControllerType.XBOX;
+        } else if (controller.getName().contains("PS")){
+            lastUsedType = ControllerType.PLAYSTATION;
+        } else if (controller.getName().contains("Nintendo")){
+            lastUsedType = ControllerType.NINTENDO;
+        } else {
+            lastUsedType = ControllerType.OTHER;
+        }
+    }
 
-	@Override
-	public void connected(Controller controller) {
-		setControllerType(controller);
-	}
+    private static boolean initialized = false;
+    private static boolean failedInit = false;
 
-	@Override
-	public void disconnected(Controller controller) {
+    public static boolean controllersSupported() {
+        if (DeviceCompat.isAndroid() && Gdx.app.getVersion() < 16) {
+            return false;
+        } else if (failedInit) {
+            return false;
+        } else if (initialized){
+            return true;
+        } else {
+            try {
+                //we do this to call Controllers.initialize(), which can fail in certain cases
+                // e.g. missing natives on very old 32-bit desktop platforms
+                Controllers.getCurrent();
+                initialized = true;
+                return true;
+            } catch (Exception e){
+                failedInit = true;
+                return false;
+            }
+        }
+    }
 
-	}
+    public static boolean vibrationSupported(){
+        return isControllerConnected() && Controllers.getCurrent().canVibrate();
+    }
 
-	@Override
-	public boolean buttonDown(Controller controller, int buttonCode) {
-		setControllerType(controller);
-		int keyCode = buttonToKey(controller, buttonCode);
-		if (keyCode != Input.Keys.UNKNOWN){
-			KeyEvent.addKeyEvent(new KeyEvent(keyCode, true));
-			return true;
-		}
-		return false;
-	}
+    public static void vibrate( int millis ){
+        if (vibrationSupported()) {
+            Controllers.getCurrent().startVibration(millis, 1f);
+        }
+    }
 
-	@Override
-	public boolean buttonUp(Controller controller, int buttonCode) {
-		setControllerType(controller);
-		int keyCode = buttonToKey(controller, buttonCode);
-		if (keyCode != Input.Keys.UNKNOWN){
-			KeyEvent.addKeyEvent(new KeyEvent(keyCode, false));
-			return true;
-		}
-		return false;
-	}
+    public static boolean isControllerConnected(){
+        return controllersSupported() && Controllers.getCurrent() != null;
+    }
 
-	public static PointF leftStickPosition = new PointF();
-	public static PointF rightStickPosition = new PointF();
+    @Override
+    public void connected(Controller controller) {
+        controllerActive = true;
+        setControllerType(controller);
+    }
 
-	private float L2Trigger = 0f;
-	private float R2Trigger = 0f;
+    @Override
+    public void disconnected(Controller controller) {
 
-	//FIXME these axis mappings seem to be wrong on Android (and iOS?) in some cases
-	@Override
-	public boolean axisMoved(Controller controller, int axisCode, float value) {
-		setControllerType(controller);
-		ControllerMapping mapping = controller.getMapping();
-		if (mapping.axisRightX == axisCode)         rightStickPosition.x = value;
-		else if (mapping.axisRightY == axisCode)    rightStickPosition.y = value;
-		else if (mapping.axisLeftX == axisCode)     leftStickPosition.x = value;
-		else if (mapping.axisLeftY == axisCode)     leftStickPosition.y = value;
+    }
 
-		//L2 and R2 triggers on Desktop
-		else if (axisCode == 4) {
+    @Override
+    public boolean buttonDown(Controller controller, int buttonCode) {
+        setControllerType(controller);
+        controllerActive = true;
+        int keyCode = buttonToKey(controller, buttonCode);
+        if (keyCode != Input.Keys.UNKNOWN){
+            KeyEvent.addKeyEvent(new KeyEvent(keyCode, true));
+            return true;
+        }
+        return false;
+    }
 
-			if (L2Trigger < 0.5f && value >= 0.5f){
-				KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_L2, true));
-			} else if (L2Trigger >= 0.5f && value < 0.5f){
-				KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_L2, false));
-			}
-			L2Trigger = value;
+    @Override
+    public boolean buttonUp(Controller controller, int buttonCode) {
+        setControllerType(controller);
+        controllerActive = true;
+        int keyCode = buttonToKey(controller, buttonCode);
+        if (keyCode != Input.Keys.UNKNOWN){
+            KeyEvent.addKeyEvent(new KeyEvent(keyCode, false));
+            return true;
+        }
+        return false;
+    }
 
-		} else if (axisCode == 5){
+    public static PointF leftStickPosition = new PointF();
+    public static PointF rightStickPosition = new PointF();
 
-			if (R2Trigger < 0.5f && value >= 0.5f){
-				KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_R2, true));
-			} else if (R2Trigger >= 0.5f && value < 0.5f){
-				KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_R2, false));
-			}
-			R2Trigger = value;
+    private float L2Trigger = 0f;
+    private float R2Trigger = 0f;
 
-		}
-		return true;
-	}
+    @Override
+    public boolean axisMoved(Controller controller, int axisCode, float value) {
+        setControllerType(controller);
+        ControllerMapping mapping = controller.getMapping();
+        if (mapping.axisRightX == axisCode)         rightStickPosition.x = value;
+        else if (mapping.axisRightY == axisCode)    rightStickPosition.y = value;
+        else if (mapping.axisLeftX == axisCode)     leftStickPosition.x = value;
+        else if (mapping.axisLeftY == axisCode)     leftStickPosition.y = value;
 
-	//we use a separate variable as Gdx.input.isCursorCatched only works on desktop
-	private static boolean controllerPointerActive = false;
+            //L2 and R2 triggers on Desktop
+        else if (axisCode == 4 && Gdx.app.getType() == Application.ApplicationType.Desktop && L2Trigger != value) {
 
-	public static void setControllerPointer( boolean active ){
-		Gdx.input.setCursorCatched(active);
-		controllerPointerActive = active;
-	}
+            if (value == 1){
+                KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_L2, true));
+                controllerActive = true;
+            } else if (value == 0){
+                KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_L2, false));
+                controllerActive = true;
+            }
+            L2Trigger = value;
 
-	public static boolean controllerPointerActive(){
-		return controllerPointerActive;
-	}
+        } else if (axisCode == 5 && Gdx.app.getType() == Application.ApplicationType.Desktop && R2Trigger != value){
 
-	//converts controller button codes to keyEvent codes
-	public static int buttonToKey(Controller controller, int btnCode){
-		ControllerMapping mapping = controller.getMapping();
-		if (btnCode == mapping.buttonA)         return Input.Keys.BUTTON_A;
-		if (btnCode == mapping.buttonB)         return Input.Keys.BUTTON_B;
-		//C button?
-		if (btnCode == mapping.buttonX)         return Input.Keys.BUTTON_X;
-		if (btnCode == mapping.buttonY)         return Input.Keys.BUTTON_Y;
-		if (btnCode == mapping.buttonBack)      return Input.Keys.BUTTON_SELECT;
-		if (btnCode == mapping.buttonStart)     return Input.Keys.BUTTON_START;
+            if (value == 1){
+                KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_R2, true));
+                controllerActive = true;
+            } else if (value == 0){
+                KeyEvent.addKeyEvent(new KeyEvent(Input.Keys.BUTTON_R2, false));
+                controllerActive = true;
+            }
+            R2Trigger = value;
 
-		if (btnCode == mapping.buttonL1)        return Input.Keys.BUTTON_L1;
-		if (btnCode == mapping.buttonL2)        return Input.Keys.BUTTON_L2;
-		if (btnCode == mapping.buttonR1)        return Input.Keys.BUTTON_R1;
-		if (btnCode == mapping.buttonR2)        return Input.Keys.BUTTON_R2;
+        }
+        return true;
+    }
 
-		if (btnCode == mapping.buttonDpadUp)    return Input.Keys.DPAD_UP;
-		if (btnCode == mapping.buttonDpadLeft)  return Input.Keys.DPAD_LEFT;
-		if (btnCode == mapping.buttonDpadDown)  return Input.Keys.DPAD_DOWN;
-		if (btnCode == mapping.buttonDpadRight) return Input.Keys.DPAD_RIGHT;
+    //we use a separate variable as Gdx.input.isCursorCatched only works on desktop
+    private static boolean controllerPointerActive = false;
+    private static PointF controllerPointerPos;
 
-		if (btnCode == mapping.buttonLeftStick) return Input.Keys.BUTTON_THUMBL;
-		if (btnCode == mapping.buttonRightStick)return Input.Keys.BUTTON_THUMBR;
+    public static void setControllerPointer( boolean active ){
+        if (active) controllerActive = true;
+        if (controllerPointerActive == active) return;
+        controllerPointerActive = active;
+        if (active){
+            Gdx.input.setCursorCatched(true);
+            controllerPointerPos = new PointF(PointerEvent.currentHoverPos());
+        } else if (!Cursor.isCursorCaptured()) {
+            Gdx.input.setCursorCatched(false);
+            Gdx.input.setCursorPosition((int)controllerPointerPos.x, (int)controllerPointerPos.y);
+        }
+    }
 
-		return Input.Keys.UNKNOWN;
-	}
+    public static boolean controllerPointerActive(){
+        return controllerPointerActive && !Cursor.isCursorCaptured();
+    }
 
-	public static boolean icControllerKey(int keyCode){
-		if (keyCode >= Input.Keys.BUTTON_A && keyCode <= Input.Keys.BUTTON_MODE){
-			return true;
-		}
+    public static PointF getControllerPointerPos(){
+        return controllerPointerPos.clone();
+    }
 
-		else if (keyCode >= Input.Keys.DPAD_UP && keyCode <= Input.Keys.DPAD_LEFT){
-			return true;
-		}
+    public static void updateControllerPointer(PointF pos, boolean sendEvent){
+        controllerPointerPos.set(pos);
+        if (sendEvent) {
+            controllerActive = true;
+            PointerEvent.addPointerEvent(new PointerEvent((int) controllerPointerPos.x, (int) controllerPointerPos.y, 10_000, PointerEvent.Type.HOVER, PointerEvent.NONE));
+        } else {
+            PointerEvent.setHoverPos(pos);
+        }
+    }
 
-		return false;
-	}
+    //we add 1000 to make the DPAD keys distinct from Keys.UP, Keys.DOWN, etc..
+    public static int DPAD_KEY_OFFSET = 1000;
 
-	public static String customButtonName(int keyCode){
-		if (lastUsedType == ControllerType.PLAYSTATION){
-			if (keyCode == Input.Keys.BUTTON_A){
-				return "Circle Button";
-			} else if (keyCode == Input.Keys.BUTTON_B){
-				return "Cross Button";
-			} else if (keyCode == Input.Keys.BUTTON_X){
-				return "Square Button";
-			} else if (keyCode == Input.Keys.BUTTON_Y){
-				return "Triangle Button";
-			}
-		}
+    //converts controller button codes to keyEvent codes
+    public static int buttonToKey(Controller controller, int btnCode){
+        ControllerMapping mapping = controller.getMapping();
+        if (btnCode == mapping.buttonA)         return Input.Keys.BUTTON_A;
+        if (btnCode == mapping.buttonB)         return Input.Keys.BUTTON_B;
+        //C button?
+        if (btnCode == mapping.buttonX)         return Input.Keys.BUTTON_X;
+        if (btnCode == mapping.buttonY)         return Input.Keys.BUTTON_Y;
+        if (btnCode == mapping.buttonBack)      return Input.Keys.BUTTON_SELECT;
+        if (btnCode == mapping.buttonStart)     return Input.Keys.BUTTON_START;
 
-		return null;
-	}
+        if (btnCode == mapping.buttonL1)        return Input.Keys.BUTTON_L1;
+        if (btnCode == mapping.buttonL2)        return Input.Keys.BUTTON_L2;
+        if (btnCode == mapping.buttonR1)        return Input.Keys.BUTTON_R1;
+        if (btnCode == mapping.buttonR2)        return Input.Keys.BUTTON_R2;
+
+        if (btnCode == mapping.buttonDpadUp)    return Input.Keys.DPAD_UP       + DPAD_KEY_OFFSET;
+        if (btnCode == mapping.buttonDpadDown)  return Input.Keys.DPAD_DOWN     + DPAD_KEY_OFFSET;
+        if (btnCode == mapping.buttonDpadLeft)  return Input.Keys.DPAD_LEFT     + DPAD_KEY_OFFSET;
+        if (btnCode == mapping.buttonDpadRight) return Input.Keys.DPAD_RIGHT    + DPAD_KEY_OFFSET;
+
+        if (btnCode == mapping.buttonLeftStick) return Input.Keys.BUTTON_THUMBL;
+        if (btnCode == mapping.buttonRightStick)return Input.Keys.BUTTON_THUMBR;
+
+        return Input.Keys.UNKNOWN;
+    }
+
+    public static boolean icControllerKey(int keyCode){
+        if (keyCode >= Input.Keys.BUTTON_A
+                && keyCode <= Input.Keys.BUTTON_MODE){
+            return true;
+        }
+
+        if (keyCode >= Input.Keys.DPAD_UP+DPAD_KEY_OFFSET
+                && keyCode <= Input.Keys.DPAD_RIGHT+DPAD_KEY_OFFSET){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String customButtonName(int keyCode){
+        if (lastUsedType == ControllerType.PLAYSTATION){
+            if (keyCode == Input.Keys.BUTTON_A){
+                return "Cross Button";
+            } else if (keyCode == Input.Keys.BUTTON_B){
+                return "Circle Button";
+            } else if (keyCode == Input.Keys.BUTTON_X){
+                return "Square Button";
+            } else if (keyCode == Input.Keys.BUTTON_Y){
+                return "Triangle Button";
+            }
+        } else if (lastUsedType == ControllerType.XBOX){
+            if (keyCode == Input.Keys.BUTTON_L1){
+                return "Left Bumper";
+            } else if (keyCode == Input.Keys.BUTTON_L2){
+                return "Left Trigger";
+            } else if (keyCode == Input.Keys.BUTTON_R1){
+                return "Right Bumper";
+            } else if (keyCode == Input.Keys.BUTTON_R2){
+                return "Right Trigger";
+            }
+        }
+
+        if (keyCode == Input.Keys.DPAD_UP + DPAD_KEY_OFFSET){
+            return Input.Keys.toString(Input.Keys.DPAD_UP);
+        } else if (keyCode == Input.Keys.DPAD_DOWN + DPAD_KEY_OFFSET){
+            return Input.Keys.toString(Input.Keys.DPAD_DOWN);
+        } else if (keyCode == Input.Keys.DPAD_LEFT + DPAD_KEY_OFFSET){
+            return Input.Keys.toString(Input.Keys.DPAD_LEFT);
+        } else if (keyCode == Input.Keys.DPAD_RIGHT + DPAD_KEY_OFFSET){
+            return Input.Keys.toString(Input.Keys.DPAD_RIGHT);
+        }
+
+        return null;
+    }
 }
--- SPD-classes/src/main/java/com/watabou/noosa/ui/Cursor.java
+++ SPD-classes/src/main/java/com/watabou/noosa/ui/Cursor.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2012-2015 Oleg Dolya
  *
  * Shattered Pixel Dungeon
- * Copyright (C) 2014-2022 Evan Debenham
+ * Copyright (C) 2014-2024 Evan Debenham
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,66 +24,100 @@ package com.watabou.noosa.ui;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.watabou.input.ControllerHandler;
+import com.watabou.noosa.Game;
+import com.watabou.utils.DeviceCompat;
 import com.watabou.utils.FileUtils;
+import com.watabou.utils.PointF;
 
 public class Cursor {
 
-	private static com.badlogic.gdx.graphics.Cursor currentCursor;
+    private static com.badlogic.gdx.graphics.Cursor currentCursor;
 
-	public enum Type {
+    public enum Type {
 
-		//TODO if we ever add more cursors, should cache their pixmaps rather than always remaking
-		DEFAULT("gdx/cursor_mouse.png"),
-		CONTROLLER("gdx/cursor_controller.png");
+        //TODO if we ever add more cursors, should cache their pixmaps rather than always remaking
+        DEFAULT("gdx/cursor_mouse.png"),
+        CONTROLLER("gdx/cursor_controller.png");
 
-		public final String file;
+        public final String file;
 
-		Type(String file){
-			this.file = file;
-		}
+        Type(String file){
+            this.file = file;
+        }
 
-	}
+    }
 
-	private static Type lastType;
-	private static int lastZoom;
+    private static Type lastType;
+    private static int lastZoom;
 
-	public static void setCustomCursor(Type type, int zoom){
+    public static void setCustomCursor(Type type, int zoom){
 
-		if (currentCursor != null){
-			if (lastType == type && lastZoom == zoom){
-				return;
-			}
+        //custom cursors (i.e. images which replace the mouse icon) are only supported on desktop
+        if (!DeviceCompat.isDesktop()){
+            return;
+        }
 
-			currentCursor.dispose();
-			currentCursor = null;
-		}
+        if (currentCursor != null){
+            if (lastType == type && lastZoom == zoom){
+                return;
+            }
 
-		Pixmap cursorImg = new Pixmap(FileUtils.getFileHandle(Files.FileType.Internal, type.file));
+            currentCursor.dispose();
+            currentCursor = null;
+        }
 
-		int scaledWidth = cursorImg.getWidth()*zoom;
-		int width2 = 2;
-		while (width2 < scaledWidth) {
-			width2 <<= 1;
-		}
+        Pixmap cursorImg = new Pixmap(FileUtils.getFileHandle(Files.FileType.Internal, type.file));
 
-		int scaledHeight = cursorImg.getHeight()*zoom;
-		int height2 = 2;
-		while (height2 < scaledHeight) {
-			height2 <<= 1;
-		}
+        int scaledWidth = cursorImg.getWidth()*zoom;
+        int width2 = 2;
+        while (width2 < scaledWidth) {
+            width2 <<= 1;
+        }
 
-		Pixmap scaledImg = new Pixmap(width2, height2, cursorImg.getFormat());
-		scaledImg.setFilter(Pixmap.Filter.NearestNeighbour);
-		scaledImg.drawPixmap(cursorImg, 0, 0, cursorImg.getWidth(), cursorImg.getHeight(), 0, 0, scaledWidth, scaledHeight);
+        int scaledHeight = cursorImg.getHeight()*zoom;
+        int height2 = 2;
+        while (height2 < scaledHeight) {
+            height2 <<= 1;
+        }
 
-		currentCursor = Gdx.graphics.newCursor(scaledImg, 0, 0);
-		Gdx.graphics.setCursor(currentCursor);
-		scaledImg.dispose();
-		cursorImg.dispose();
+        Pixmap scaledImg = new Pixmap(width2, height2, cursorImg.getFormat());
+        scaledImg.setFilter(Pixmap.Filter.NearestNeighbour);
+        scaledImg.drawPixmap(cursorImg, 0, 0, cursorImg.getWidth(), cursorImg.getHeight(), 0, 0, scaledWidth, scaledHeight);
 
-		lastType = type;
-		lastZoom = zoom;
+        currentCursor = Gdx.graphics.newCursor(scaledImg, 0, 0);
+        Gdx.graphics.setCursor(currentCursor);
+        scaledImg.dispose();
+        cursorImg.dispose();
 
-	}
+        lastType = type;
+        lastZoom = zoom;
+
+    }
+
+    private static boolean cursorCaptured = false;
+
+    public static void captureCursor(boolean captured){
+        cursorCaptured = captured;
+
+        if (captured) {
+            Gdx.input.setCursorCatched(true);
+        } else {
+            if (ControllerHandler.controllerPointerActive()) {
+                ControllerHandler.setControllerPointer(true);
+                ControllerHandler.updateControllerPointer(new PointF(Game.width/2f, Game.height/2f), false);
+            } else {
+                Gdx.input.setCursorCatched(false);
+            }
+        }
+    }
+
+    public static PointF getCursorDelta(){
+        return new PointF(Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+    }
+
+    public static boolean isCursorCaptured(){
+        return cursorCaptured;
+    }
 
 }
